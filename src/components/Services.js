fafeb38436b91299e5de217aa00c98fd949729cf
Mark Gire <markgire@Marks-MacBook-Pro.local>
2021-09-14 02:36:19 -0400

    Created Services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,69 @@
 import React, { useState } from "react";
-import { Grid, Paper, Button, Drawer, List, ListItem, Typography, IconButton } from "@material-ui/core";
+import { Grid, Paper, Button, Drawer, List, ListItem, Typography, IconButton, Card, CardMedia, CardHeader } from "@material-ui/core";
 
 import '@fontsource/roboto';
 
 const Services = () => {
+
+    const style = {
+        body: {
+
+        },
+        prenatal: {
+            backgroundColor: "#FFCFED",
+            color: "#630070",
+            margin: "2%",
+            padding: "5%"
+        },
+        labor: {
+            backgroundColor: "#630070",
+            color: "#FFCFED",
+            margin: "2%",
+            padding: "5%"
+        },
+        newborn: {
+            backgroundColor: "#FFCFED",
+            color: "#630070",
+            margin:"2%",
+            padding: "5%"
+        }
+    }
+
     return (
-        <>
-            <Typography variant="h5">Services</Typography>
-        </>
+        <Grid container direction="row" alignItems="center" justifyContent="center">
+            <Grid item md="4" xs="12">
+                <Card style={style.prenatal}>
+                    <CardHeader title="Prenatal" />
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                </Card>
+            </Grid>
+            <Grid item md="4" xs="12">
+                <Card style={style.labor}>
+                    <CardHeader title="Labor and Birth" />
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+
+                </Card>
+            </Grid>
+            <Grid item md="4" xs="12">
+                <Card style={style.newborn}>
+                    <CardHeader title="Newborn" />
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                    <Typography>SomeTextHere</Typography>
+                </Card>
+            </Grid>
+            
+        </Grid>
     )
 }
 
